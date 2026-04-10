a6d80d4feed312e2d2983963b6a9fbc0577a8385
Devansh Tiwari <devanshtiwari817@gmail.com>
2026-04-10 08:44:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 # ------------------------------
 @st.cache_resource
 def load_resources():
-    model = load_model("lstm_model (1).h5")
+    model = load_model("lstm_model.h5")
     with open("tokenizer.pkl", "rb") as f:
         tokenizer = pickle.load(f)
     with open("max_len.pkl", "rb") as f:
